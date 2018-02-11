a80cdedfdf4d1c2e62e458fad21da2084a3501a8
Jerry Morrison <1fish2@users.noreply.github.com>
2018-02-11 15:09:29 -0800

    add robot driving and auto-run scoring
    … with travel times between locations.
    
    Make class Plate simpler, not an Actor.
    
    Running the simulation now scores (15, 15) points.

--- simulation.py
+++ simulation.py
@@ -46,14 +46,16 @@ class Color(str):
 
 
 # Singleton alliance Color objects.
-RED, BLUE = Color('red'), Color('blue')
+RED, BLUE = Color('RED'), Color('BLUE')
 RED.opposite, BLUE.opposite = BLUE, RED
 
 
 # Robot locations. Cubes can also be in these locations and in Robots,
 # Switch plates, Scale plates, and Vault columns, but not *_PLATFORM_CLIMBED.
 # The Scoring Table is at the "back side."
-# An "outer zone" is between the alliance wall and the auto-line.
+# The red/blue "outer zone" is between the alliance wall and the auto-line.
+#
+# TODO: Split these finer, esp. front/back outer zone?
 Location = Enum(
     'Location',
     'RED_EXCHANGE_ZONE BLUE_EXCHANGE_ZONE '
@@ -61,13 +63,67 @@ Location = Enum(
     'RED_POWER_CUBE_ZONE BLUE_POWER_CUBE_ZONE '
     'RED_SWITCH_FENCE BLUE_SWITCH_FENCE '
     'RED_OUTER_ZONE BLUE_OUTER_ZONE '
-    'RED_FRONT_INNER_ZONE RED_BACK_INNER_ZONE BLUE_FRONT_INNER_ZONE  BLUE_BACK_INNER_ZONE '
+    'RED_FRONT_INNER_ZONE RED_BACK_INNER_ZONE BLUE_FRONT_INNER_ZONE BLUE_BACK_INNER_ZONE '
     'RED_PLATFORM BLUE_PLATFORM RED_PLATFORM_CLIMBED BLUE_PLATFORM_CLIMBED '
     'FRONT_NULL_TERRITORY BACK_NULL_TERRITORY ')
 
 for loc in Location:
+    loc.is_inner_zone = loc.name.endswith('_INNER_ZONE')
     loc.cubes = 0  # The number of cubes in this Location.
     loc.adjacent_plate = None  # Adjacent seesaw Plate to place Cubes.
+
+ScoreFactor = Enum('ScoreFactor', 'NOT_YET ACHIEVED COUNTED')
+
+TRAVEL_TIMES = dict()  # map from (location1, location2) -> Robot travel time in seconds
+
+
+def _init_travel_times():
+    """
+    Initialize travel times for direct paths. In this simulation, a Robot will
+    jump to the destination after this many seconds. Take longer routes in a
+    sequence of direct paths via intermediate Locations *OUTER_ZONE,
+    *INNER_ZONE, *SWITCH_FENCE, *PLATFORM, *NULL_TERRITORY.
+
+    # TODO: Do this via loops over Location['RED_OUTER_ZONE'] et al?
+    """
+    def set_pair(location1, location2, time):
+        TRAVEL_TIMES[(location1, location2)] \
+            = TRAVEL_TIMES[(location2, location1)] = time
+
+    set_pair(Location.RED_OUTER_ZONE, Location.RED_EXCHANGE_ZONE, 2)
+    set_pair(Location.RED_OUTER_ZONE, Location.RED_FRONT_PORTAL, 5)
+    set_pair(Location.RED_OUTER_ZONE, Location.RED_BACK_PORTAL, 5)
+    set_pair(Location.RED_OUTER_ZONE, Location.RED_POWER_CUBE_ZONE, 2)
+    set_pair(Location.RED_OUTER_ZONE, Location.RED_FRONT_INNER_ZONE, 6)
+    set_pair(Location.RED_OUTER_ZONE, Location.RED_BACK_INNER_ZONE, 6)
+
+    set_pair(Location.RED_FRONT_INNER_ZONE, Location.RED_SWITCH_FENCE, 4)
+    set_pair(Location.RED_BACK_INNER_ZONE, Location.RED_SWITCH_FENCE, 4)
+    set_pair(Location.RED_FRONT_INNER_ZONE, Location.RED_PLATFORM, 4)
+    set_pair(Location.RED_BACK_INNER_ZONE, Location.RED_PLATFORM, 4)
+    set_pair(Location.RED_SWITCH_FENCE, Location.RED_PLATFORM, 2)
+
+    set_pair(Location.RED_FRONT_INNER_ZONE, Location.FRONT_NULL_TERRITORY, 6)
+    set_pair(Location.RED_BACK_INNER_ZONE, Location.BACK_NULL_TERRITORY, 6)
+
+    set_pair(Location.BLUE_OUTER_ZONE, Location.BLUE_EXCHANGE_ZONE, 2)
+    set_pair(Location.BLUE_OUTER_ZONE, Location.BLUE_FRONT_PORTAL, 5)
+    set_pair(Location.BLUE_OUTER_ZONE, Location.BLUE_BACK_PORTAL, 5)
+    set_pair(Location.BLUE_OUTER_ZONE, Location.BLUE_POWER_CUBE_ZONE, 2)
+    set_pair(Location.BLUE_OUTER_ZONE, Location.BLUE_FRONT_INNER_ZONE, 6)
+    set_pair(Location.BLUE_OUTER_ZONE, Location.BLUE_BACK_INNER_ZONE, 6)
+
+    set_pair(Location.BLUE_FRONT_INNER_ZONE, Location.BLUE_SWITCH_FENCE, 4)
+    set_pair(Location.BLUE_BACK_INNER_ZONE, Location.BLUE_SWITCH_FENCE, 4)
+    set_pair(Location.BLUE_FRONT_INNER_ZONE, Location.BLUE_PLATFORM, 4)
+    set_pair(Location.BLUE_BACK_INNER_ZONE, Location.BLUE_PLATFORM, 4)
+    set_pair(Location.BLUE_SWITCH_FENCE, Location.BLUE_PLATFORM, 2)
+
+    set_pair(Location.BLUE_FRONT_INNER_ZONE, Location.FRONT_NULL_TERRITORY, 6)
+    set_pair(Location.BLUE_BACK_INNER_ZONE, Location.BACK_NULL_TERRITORY, 6)
+
+
+_init_travel_times()
 
 
 class Score(namedtuple('Score', 'red blue')):
@@ -144,6 +200,9 @@ class Simulation(object):
             agent.update(time)
 
 
+# TODO: pickup(), drop(), and place() should take at least 1 second.
+# TODO: Disallow more than 1 action (Cube or driving) at a time, either by
+# canceling the current action or not starting another.
 class Robot(Agent):
     def __init__(self, alliance, player, location=None):
         """
@@ -157,12 +216,42 @@ class Robot(Agent):
 
         if location is None:
             location = Location.RED_OUTER_ZONE if alliance is RED else Location.BLUE_OUTER_ZONE
-        self.location = location
+        self.destination = self.location = location
+        self.eta = None  # => not driving
         self.cubes = 0
+        self.auto_run = ScoreFactor.NOT_YET
 
     def __str__(self):
+        # TODO: Include the current action and destination.
         return "Robot({}{}) in {} with {} Cube(s)".format(
             self.alliance, self.player, self.location, self.cubes)
+
+    def update(self, time):
+        super(Robot, self).update(time)
+        if self.time == self.eta:
+            self.location = self.destination
+            self.eta = None
+
+            if (self.auto_run is ScoreFactor.NOT_YET and self.autonomous
+                    and self.location.is_inner_zone):
+                self.auto_run = ScoreFactor.ACHIEVED
+
+    def score(self):
+        if self.auto_run is ScoreFactor.ACHIEVED:
+            points = Score.pick(self.alliance, 5)
+            self.auto_run = ScoreFactor.COUNTED
+        else:
+            points = Score.ZERO
+        # TODO: Add Parking or Climbing points.
+        return points
+
+    def head_to(self, destination):
+        """
+        Begin driving to the given destination. Cancel any current destination.
+        Does no path planning -- raises KeyError if destination is not adjacent.
+        """
+        self.destination = destination
+        self.eta = self.time + TRAVEL_TIMES[(self.location, destination)]
 
     def pickup(self):
         """If there's a Cube here and room in the Robot, pick it up and return True."""
@@ -195,14 +284,14 @@ class Robot(Agent):
         return False
 
 
-class Plate(Agent):
-    """A Plate holding Cubes on one side of a Scale or Switch (a "seesaw")."""
-    def __init__(self, seesaw, is_front):
-        super(Plate, self).__init__()
-        self.seesaw = seesaw
-        self.is_front = is_front
-
+class Plate(object):
+    """A Plate holding Cubes on one side of a "seesaw" (Scale or Switch)."""
+    def __init__(self, name):
+        self.name = name
         self.cubes = 0
+
+    def __str__(self):
+        return "{} {} with {} Cubes".format(self.name, type(self), self.cubes)
 
 
 class Scale(Agent):
@@ -214,8 +303,8 @@ class Scale(Agent):
         super(Scale, self).__init__()
         self.alliance_end = ''
         self.front_color = front_color
-        self.front_plate = Plate(self, True)
-        self.back_plate = Plate(self, False)
+        self.front_plate = Plate(self._plate_name("Front"))
+        self.back_plate = Plate(self._plate_name("Back"))
 
         self.forced = False
         self.force_timeout = 0
@@ -227,6 +316,9 @@ class Scale(Agent):
 
         self._setup_locations()
 
+    def _plate_name(self, front_back):
+        return "{} Scale".format(front_back)
+
     def _setup_locations(self):
         """Set the adjacent Locations to point to the Plates."""
         Location.FRONT_NULL_TERRITORY.adjacent_plate = self.front_plate
@@ -234,7 +326,7 @@ class Scale(Agent):
 
     def __str__(self):
         return "{}({}/{}) with {} Cube(s)".format(
-            self.__class__, self.alliance_end, self.front_color, self.cubes)
+            type(self), self.alliance_end, self.front_color, self.cubes)
 
     @property
     def cubes(self):
@@ -265,8 +357,6 @@ class Scale(Agent):
 
     def update(self, time):
         super(Scale, self).update(time)
-        self.front_plate.update(time)
-        self.back_plate.update(time)
 
         if self.forced and time >= self.force_timeout:
             self.forced = False
@@ -307,6 +397,9 @@ class Switch(Scale):
         """
         super(Switch, self).__init__(front_color)
         self.alliance_end = alliance_end
+
+    def _plate_name(self, front_back):
+        return "{} {} Switch".format(front_back, self.alliance_end)
 
     def _setup_locations(self):
         """Set up the adjacent Locations to point to the Plates."""
@@ -390,7 +483,6 @@ class Vault(Agent):
     An alliance's Vault for power-ups.
     Example usage: vault.force.play().
     """
-
     def __init__(self, alliance, switch, scale):
         super(Vault, self).__init__()
         self.alliance = alliance
@@ -412,19 +504,40 @@ class Vault(Agent):
         return sum((column.score() for column in self.columns), Score.ZERO)
 
 
+class RobotPlayer(object):
+    """Chooses a Robot's actions: Preload a Cube or not, driving plans, etc."""
+    def __init__(self, robot):
+        self.robot = robot
+
+        # First cut decisions: Preload Cubes in all Robots and drive to
+        # earn auto-run points.
+        robot.cubes = 1
+        destination_name = "{}_{}_INNER_ZONE".format(
+            robot.alliance, "FRONT" if robot.player < 3 else "BACK")
+        robot.head_to(Location[destination_name])
+
+    # TODO: A generator to yield Robot actions as the game proceeds.
+
+
+class HumanPlayer(object):
+    """Chooses a human player's actions."""
+    def __init__(self, alliance):
+        self.alliance = alliance
+
+    # TODO: A generator to yield human player actions as the game proceeds.
+
+
 class PowerUpGame(Simulation):
     def __init__(self):
         super(PowerUpGame, self).__init__()
         # TODO: An initial-conditions vector for the FMS choices.
-        # TODO: Red and Blue alliance gameplay decision objects.
         switch_front_color = RED
         scale_front_color = BLUE
 
+        # Create and add all the game objects.
         self.robots = [Robot(alliance, player)
                        for alliance in (RED, BLUE)
                        for player in xrange(1, 4)]
-        for robot in self.robots:
-            robot.cubes = 1  # TODO: gameplay choices
 
         red_switch = Switch(RED, switch_front_color)
         blue_switch = Switch(BLUE, switch_front_color)
@@ -436,8 +549,14 @@ class PowerUpGame(Simulation):
         for agent in chain(self.robots, self.seesaws, self.vaults.itervalues()):
             self.add(agent)
 
+        # Start keeping score.
         self.score = Score.ZERO
 
+        # Create the player decision objects.
+        self.robot_players = [RobotPlayer(robot) for robot in self.robots]
+        self.human_players = [HumanPlayer(alliance) for alliance in (RED, BLUE)]
+
+        # Place the remaining Cubes on the field now that RobotPlayers preloaded some.
         self._place_cubes()
 
     def _initial_portal_cubes(self, alliance):
